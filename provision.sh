47acf92774a6bc0fbbafa827483470750b497c51
Rui Lopes <rgl@ruilopes.com>
2018-06-15 22:12:26 +0100

    migrate to proxmox 5.2

--- provision.sh
+++ provision.sh
@@ -101,7 +101,7 @@ EOF
 # disable the "You do not have a valid subscription for this server. Please visit www.proxmox.com to get a list of available options."
 # message that appears each time you logon the web-ui.
 # NB this file is restored when you (re)install the pve-manager package.
-echo 'PVE.Utils.checked_command = function(o) { o(); };' >>/usr/share/pve-manager/js/pvemanagerlib.js
+echo 'Proxmox.Utils.checked_command = function(o) { o(); };' >>/usr/share/pve-manager/js/pvemanagerlib.js
 
 # install vim.
 apt-get install -y --no-install-recommends vim
@@ -165,10 +165,21 @@ fi
 # create the cluster or add the node to the cluster.
 # see https://pve.proxmox.com/wiki/Cluster_Manager
 if [ "$cluster_ip" == "$cluster_network_first_node_ip" ]; then
-    pvecm create example -ring0_addr $cluster_ip --bindnet0_addr $cluster_network
+    pvecm create example --ring0_addr $cluster_ip --bindnet0_addr $cluster_network
 else
-    ssh-keyscan -H $cluster_network_first_node_ip >>~/.ssh/known_hosts
-    pvecm add $cluster_network_first_node_ip -ring0_addr $cluster_ip
+    apt-get install -y --no-install-recommends expect
+    # add the node to the cluster by automatically entering the root password and accept the host SSH key fingerprint. e.g.:
+    #   pve2: Please enter superuser (root) password for '10.2.0.201':
+    #   pve2: Etablishing API connection with host '10.2.0.201'
+    #   pve2: The authenticity of host '10.2.0.201' can't be established.
+    #   pve2: X509 SHA256 key fingerprint is 4B:6A:76:6F:32:31:A5:52:D4:C9:D3:94:23:CF:DD:35:AC:6D:AC:8D:81:42:D6:51:DA:E2:CC:C9:BD:92:0C:61.
+    #   pve2: Are you sure you want to continue connecting (yes/no)? 
+    expect <<EOF
+spawn pvecm add $cluster_network_first_node_ip --ring0_addr $cluster_ip
+expect -re "Please enter superuser (root) password for .+:"; send "vagrant\\r"
+expect "Are you sure you want to continue connecting (yes/no)? "; send "yes\\r"
+expect eof
+EOF
 fi
 pvecm status || true
 pvecm nodes

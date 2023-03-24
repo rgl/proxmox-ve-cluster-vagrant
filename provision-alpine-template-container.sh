488d731bf242abcd423d8510b7ae3fa141564ac8
Rui Lopes <rgl@ruilopes.com>
2023-03-24 07:37:23 +0000

    upgrade to alpine 3.17

--- provision-alpine-template-container.sh
+++ provision-alpine-template-container.sh
@@ -18,7 +18,7 @@ pveam update
 pveam available # show templates.
 
 # download the alpine template to the iso-templates shared storage.
-pve_template=alpine-3.12-default_20200823_amd64.tar.xz
+pve_template=alpine-3.17-default_20221129_amd64.tar.xz
 pveam download iso-templates $pve_template
 
 # create and start a container.
